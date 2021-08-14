8bb621df985d7b67e41d28c40964e613a0af7fe8
Andres Bayuelo <andres.bayuelo@hotmail.com>
2021-08-14 18:23:33 -0500

    Show All Posts Associated With a Category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,6 +10,9 @@
 <body>
     <article>
         <h1>{{ $post->title }}</h1>
+        <p>
+            <a href="/posts/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+        </p>
         <div>
             {!! $post->body !!}
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -36,4 +37,10 @@ Route::get('/posts/{post:slug}', function (Post $post) { // Post::where('slug', 
      return view('post', [
         'post' => $post
     ]);
+});
+
+Route::get('/posts/categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+       'posts' => $category->posts
+   ]);
 });
